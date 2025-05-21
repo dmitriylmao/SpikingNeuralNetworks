003ddaf0c95e0cfdf4a3b6b543abae80349765ea
Елецкий Дмитрий <dimaeleckij2016@gmail.com>
2025-05-21 21:29:17 +0300

    Setting up the MNIST Dataset
    1.1. Import packages and setup environment
    1.2 Download Dataset
    1.3 Create DataLoaders

---
+++ tutorial_1.py
@@ -0,0 +1,34 @@
+import snntorch as snn
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from snntorch import utils
+
+batch_size = 128 #это партия данных
+data_path = './data/mnist'  # путь куда будут скачиваться данные
+num_classes = 10  # 10 цифр от 0 до 9
+dtype = torch.float
+
+# Преобразования: 28x28, grayscale, тензор, нормализация от 0 до 1
+transform = transforms.Compose([
+    transforms.Resize((28, 28)),
+    transforms.Grayscale(),
+    transforms.ToTensor(),
+    transforms.Normalize((0,), (1,))
+])
+
+# Загрузка тренировочного датасета
+mnist_train = datasets.MNIST(data_path, train=True, download=True, transform=transform)
+
+subset = 10  # уменьшаем в 10 раз (6000 вместо 60000)
+mnist_train = utils.data_subset(mnist_train, subset)
+print(f"The size of mnist_train is {len(mnist_train)}")
+
+#итератор, который выдаёт батчи по 128 изображений
+train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True)
+
+dataiter = iter(train_loader)
+images, labels = next(dataiter)
+
+print(images.shape)  # torch.Size([128, 1, 28, 28])
+print(labels.shape)  # torch.Size([128])
